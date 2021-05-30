0d2270a48111adcd2b24032a8db9cdad26d4f26e
josemj96 <josec.molinajimenez@gmail.com>
2021-05-30 09:57:33 +0200

    Comentarios Quitados

--- src/main/java/com/Marca/marca/LoadDatabase.java
+++ src/main/java/com/Marca/marca/LoadDatabase.java
@@ -10,41 +10,8 @@ import org.springframework.context.annotation.Configuration;
 class LoadDatabase {
 
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
-    /**
-    @Bean
-    CommandLineRunner initDatabase(AddressRepository addRepo, LibraryRepository libRepo, BookRepository bookRepo, AuthorRepository authRepo) {
 
-        return args -> {
-            Address address = new Address("Avenida de Burgos 123");
-            addRepo.save(address);
-            Library library = new Library("La gran biblioteca");
-            libRepo.save(library);
-            library.setAddress(address);
-            address.setLibrary(library);
-            addRepo.save(address);
-            libRepo.save(library);
-            Book book = new Book("El señor de los anillos");
-            book.setLibrary(library);
-            library.addBook(book);
-            libRepo.save(library);
-            bookRepo.save(book);
-            Author author = new Author("J.R.R. Tolkien");
-            authRepo.save(author);
-            author.addBook(book);
-            book.addAuthor(author);
-            bookRepo.save(book);
-            log.info("Preloading " + authRepo.save(author));
-            
-            Address address2 = new Address("Avenida de Ciudad de Barcelona 123");
-            addRepo.save(address2);
-            Library library2 = new Library("Otra gran biblioteca");
-            libRepo.save(library2);
-            Book book2 = new Book("Mistborn");
-            bookRepo.save(book2);
-            Author author2 = new Author("Brandon Sanderson");
-            authRepo.save(author2);
-        };**/
-        
+    
         @Bean
         CommandLineRunner initDatabase(MarcaRepository marRepo, ProductoRepository proRepo) {
 
